06b7b69d1e5621ff374f7e9db496d3b59c873109
Krsts <42552045+Krsts@users.noreply.github.com>
2019-01-24 23:59:22 +0100

    Alternate Color broken

--- src/app/appartement-list-item/appartement-list-item.component.ts
+++ src/app/appartement-list-item/appartement-list-item.component.ts
@@ -2,6 +2,7 @@ import { AppartementJson } from './../shared/appartement-json.model';
 // import { AppartementService } from './../services/appartement.service';
 // import { Appartement } from './../shared/appartement.model';
 import { Component, OnInit, Input } from '@angular/core';
+import { booleanLiteral } from 'babel-types';
 
 @Component({
   selector: 'app-appartement-list-item',
@@ -13,14 +14,28 @@ export class AppartementListItemComponent implements OnInit {
 
 @Input() appartement: AppartementJson[];
 
+colorSwitch : boolean;
+
+
 
   // constructor(private appartementService: AppartementService) { }
     constructor () {}
 
   ngOnInit() {
-    
+    this.colorSwitch = true;
   }
 
+
+  alternatecolor() {
+    this.colorSwitch = !this.colorSwitch;
+    if (this.colorSwitch === false){
+      this.colorSwitch = !this.colorSwitch;
+    return "#FE4B74";}
+    else{
+      this.colorSwitch = !this.colorSwitch;
+      return "green";}
+    
+  }
   // onSelected() {
   //   this.appartementService.appartementSelected.emit(this.appartement);
   // }
